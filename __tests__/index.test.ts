e06a983c53b4dfbeaaa005c1526948316ee9320d
Matthew Belisle <matthew.belisle@workiva.com>
2025-04-11 12:41:32 -0500

    Cleanup index.tests.js

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,11 +1,8 @@
 import { jest } from '@jest/globals'
 
 jest.unstable_mockModule('../src/main.js', () => ({
-  // Mock the action's entrypoint.
   run: jest.fn(() => Promise.resolve()),
 }))
-
-// Modules to be mocked.
 const main = await import('../src/main.js')
 
 describe('index', () => {
